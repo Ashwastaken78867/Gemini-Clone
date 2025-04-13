bd854d88f5cd1950fb400b74ebd18271738f2cf1
Ash Bagda <abagda34@gmail.com>
2025-04-13 16:59:47 +0530

    Fix: added /chat route and updated navigation

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router';
 import './Header.css'; 
+import Chat from '../../pages/chat';
 
 const Header = (props) => {
   const { logo } = props;
@@ -10,11 +11,10 @@ const Header = (props) => {
       
       <nav className="nav">
         <ul className="nav_items">
-        <li><Link to="/chat">Chat</Link></li>
-
           <li><Link to="/about">About</Link></li>
           <li><Link to="/teams">Team</Link></li>
           <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/chat">Chat</Link></li>
         </ul>
       </nav>
     </header>
